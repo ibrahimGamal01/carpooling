94899415b1428b00df280cc7d8e3ef36581d44b7
ibrahimGamal01 <ibrahim.moustafa@ejust.edu.eg>
2023-07-19 16:18:46 +0300

    Update fetching and creating rides using Geocoding API

--- php/create_ride.php
+++ php/create_ride.php
@@ -13,21 +13,53 @@ if ($conn->connect_error) {
 
 // Retrieve ride details from the request
 $requestData = json_decode(file_get_contents('php://input'), true);
-$pickup = isset($requestData['pickup']) ? $requestData['pickup'] : '';
-$dropoff = isset($requestData['dropoff']) ? $requestData['dropoff'] : '';
+$pickupLocation = isset($requestData['pickupLocation']) ? $requestData['pickupLocation'] : '';
+$dropoffLocation = isset($requestData['dropoffLocation']) ? $requestData['dropoffLocation'] : '';
 $seats = isset($requestData['seats']) ? $requestData['seats'] : '';
 
 // Validate the required fields
-if (empty($pickup) || empty($dropoff) || empty($seats)) {
+if (empty($pickupLocation) || empty($dropoffLocation) || empty($seats)) {
   die("Error: Required fields are missing.");
+}
+
+// Function to get coordinates using PositionStack Geocoding API
+function getCoordinates($query) {
+  $apiKey = '54852a7462e1c40cc8fca727234d19cf';
+  $apiUrl = 'http://api.positionstack.com/v1/forward?access_key=' . $apiKey . '&query=' . urlencode($query);
+
+  // Make the API request using cURL
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $apiUrl);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  // Parse the API response to extract latitude and longitude
+  $responseData = json_decode($response, true);
+  if (isset($responseData['data'][0]['latitude']) && isset($responseData['data'][0]['longitude'])) {
+    return array(
+      'latitude' => $responseData['data'][0]['latitude'],
+      'longitude' => $responseData['data'][0]['longitude']
+    );
+  }
+
+  return null; // Return null if coordinates not found
 }
 
 // Get the driver's ID (You can modify this part to match your authentication system)
 $driverId = 13; // Assuming the driver's ID is 13
 
+// Get coordinates for pickup and dropoff locations
+$pickupCoordinates = getCoordinates($pickupLocation);
+$dropoffCoordinates = getCoordinates($dropoffLocation);
+
+if (!$pickupCoordinates || !$dropoffCoordinates) {
+  die("Error: Failed to get coordinates for pickup or drop-off location.");
+}
+
 // Prepare and execute the SQL query to create a new ride
-$stmt = $conn->prepare("INSERT INTO rides (driver_id, pickup_location, dropoff_location, available_seats, status) VALUES (?, ?, ?, ?, 'upcoming')");
-$stmt->bind_param("isss", $driverId, $pickup, $dropoff, $seats);
+$stmt = $conn->prepare("INSERT INTO rides (driver_id, pickup_location, pickup_latitude, pickup_longitude, dropoff_location, dropoff_latitude, dropoff_longitude, available_seats, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'upcoming')");
+$stmt->bind_param("issssssi", $driverId, $pickupLocation, $pickupCoordinates['latitude'], $pickupCoordinates['longitude'], $dropoffLocation, $dropoffCoordinates['latitude'], $dropoffCoordinates['longitude'], $seats);
 
 if ($stmt->execute()) {
   // Get the newly created ride's ID
@@ -41,8 +73,12 @@ if ($stmt->execute()) {
   $response = array(
     'rideId' => $rideId,
     'driver' => 'John Doe', // Replace with the actual driver's name
-    'pickup' => $pickup,
-    'dropoff' => $dropoff,
+    'pickup' => $pickupLocation,
+    'pickupLatitude' => $pickupCoordinates['latitude'],
+    'pickupLongitude' => $pickupCoordinates['longitude'],
+    'dropoff' => $dropoffLocation,
+    'dropoffLatitude' => $dropoffCoordinates['latitude'],
+    'dropoffLongitude' => $dropoffCoordinates['longitude'],
     'seats' => $seats
   );
 
--- php/fetch_rides.php
+++ php/fetch_rides.php
@@ -17,21 +17,60 @@ $requestData = json_decode(file_get_contents('php://input'), true);
 $pickupLocation = $requestData['pickup'];
 $dropoffLocation = $requestData['dropoff'];
 
-// Fetch the matching rides based on pickup and drop-off locations
-$sql = "SELECT * FROM rides WHERE pickup_location LIKE ? AND dropoff_location LIKE ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $pickupLocation, $dropoffLocation);
-$stmt->execute();
-$result = $stmt->get_result();
+// Calculate the maximum allowable distance in kilometers (5 kilometers)
+$maxDistance = 5;
+
+// Fetch all rides from the database
+$sql = "SELECT * FROM rides";
+$result = $conn->query($sql);
 $rides = [];
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
+while ($row = $result->fetch_assoc()) {
+    $pickupLat = $row['pickup_latitude'];
+    $pickupLon = $row['pickup_longitude'];
+    $dropoffLat = $row['dropoff_latitude'];
+    $dropoffLon = $row['dropoff_longitude'];
+    $rideId = $row['ride_id'];
+
+    // Check for an exact match (same pickup and drop-off)
+    if ($pickupLocation === $row['pickup_location'] && $dropoffLocation === $row['dropoff_location']) {
         $rides[] = $row;
+    } else {
+        // Calculate the distance between the pickup and drop-off locations of the ride and the requested locations
+        function haversineDistance($lat1, $lon1, $lat2, $lon2) {
+            $earthRadius = 6371; // Radius of the Earth in kilometers
+            $dLat = deg2rad($lat2 - $lat1);
+            $dLon = deg2rad($lon2 - $lon1);
+            $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon / 2) * sin($dLon / 2);
+            $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+            $distance = $earthRadius * $c;
+            return $distance;
+        }
+
+        $pickupDistance = haversineDistance($pickupLat, $pickupLon, $requestData['pickup_latitude'], $requestData['pickup_longitude']);
+        $dropoffDistance = haversineDistance($dropoffLat, $dropoffLon, $requestData['dropoff_latitude'], $requestData['dropoff_longitude']);
+
+        // Check for on the way match
+        if ($pickupDistance <= $maxDistance && $dropoffDistance <= $maxDistance) {
+            $rides[] = $row;
+        } else {
+            // Check for same drop-off match
+            if ($dropoffLocation === $row['dropoff_location']) {
+                $rides[] = $row;
+            } else {
+                // Check for same pickup match
+                if ($pickupLocation === $row['pickup_location']) {
+                    $rides[] = $row;
+                } else {
+                    // Check for general match (within a distance threshold)
+                    if ($pickupDistance <= $maxDistance || $dropoffDistance <= $maxDistance) {
+                        $rides[] = $row;
+                    }
+                }
+            }
+        }
     }
 }
-
-$stmt->close();
 
 // Return the matching rides as JSON response
 header('Content-Type: application/json');
